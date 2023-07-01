4030ff85745ea64db1d553cdcaae4e2679b39d8d
Taryn Aranda <129102703+tarynaranda17@users.noreply.github.com>
2023-06-30 20:36:15 -0600

    Add files via upload

---
+++ M3Lab1_ta.py
@@ -0,0 +1,54 @@
+# Guess a number from 1 to 10
+# By C. Calongne, 01/14/2019
+# Pseudocode & Python with Iteration M3Lab1_student.py
+# Guess a number from 1 to 10
+# Write the statements requested in Steps 1-6 below
+# 
+# See the examples in the provided code
+# Use structured programming and indent your code.
+# Programmer Name: *****Taryn Aranda****
+
+import random
+
+# uses randrange instead of randint for better results in Python 3.7 
+# randrange stops just before the upper range, use (1, 11) for 1-10
+
+num = random.randrange(1, 11)
+# Step 1: Ask the player to enter a name or quit to exit
+
+print('Taryn')
+
+# Step 2: use a while statement to test when the name is not equal to quit
+num = 10
+if num <8:
+        print("Guess is too high, go lower!")
+if num >7:
+        print("Guess is too low, try again!")
+
+    # Step 3: input enter a number from 1 to 10 for the variable your_guess
+
+    # display the number guessed
+print("Your number is")
+def_input(num)
+
+while num != 10:
+
+        # Step 4: Write an if statement for your_guess is less than num
+ elif number == 7:
+            print("Your guess was 7, YOU WON!")
+            10== int(input("Guess another number from 1 to 10: "))
+
+    print("The correct number was", 7)
+    # Step 5 display text with your guess and You won, name
+
+    print=("Your guess was 7, YOU WON!")
+    
+    print("***************************************************************")
+    # Step 6 ask the player to enter a name or quit to exit
+    input(quit)
+
+
+num = random.randrange(1, 11)
+print("Thank you for playing!") 
+
+
